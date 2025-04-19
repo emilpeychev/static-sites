cbbd448861feb58dc7ea3d06df4522825fb5c1c4
emilpeychev <emi.peychev@gmail.com>
2025-04-19 11:12:24 +0300

    add updates

---
+++ script.groovy
@@ -0,0 +1,18 @@
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'DockerHub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        def timestamp = sh(script: 'date +"%Y%m%d%H%M%S"', returnStdout: true).trim()
+        def imageName = "softunium/static-site:rightshift.bg${timestamp}"
+
+        echo "Image name: ${imageName}"
+        
+        sh "docker build -t ${imageName} ."
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh "docker push ${imageName}"
+    }
+}
+
+def deployApp() {
+    echo 'Deploying the application...'
+    // Add your deployment logic here
+}
