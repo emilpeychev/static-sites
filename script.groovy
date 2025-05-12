395bc0db5768b47bfa8c5b070c111241ccda2b35
emilpeychev <emi.peychev@gmail.com>
2025-05-12 12:23:10 +0300

    script

--- script.groovy
+++ script.groovy
@@ -18,12 +18,13 @@ def deployApp() {
     echo 'Deploying the application...'
     // Deployment logic here
     withCredentials([usernamePassword(credentialsId: 'DockerHub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh '''
-            echo $PASS | docker login -u $USER --password-stdin
+        sh """
+            echo \$PASS | docker login -u \$USER --password-stdin
             docker pull ${imageName}
             docker rm -f static-site || true
             docker run -d --name static-site -p 8080:80 ${imageName}
-            '''
+    """
+
     }
 }
 return this
