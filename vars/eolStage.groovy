b55359498c5cab64a672168437ca1d6878cb4cf5
emilpeychev <emi.peychev@gmail.com>
2025-05-16 11:54:41 +0300

    switch EOL to /tmp/bin

--- vars/eolStage.groovy
+++ vars/eolStage.groovy
@@ -7,11 +7,11 @@ def call() {
         if ! command -v xeol > /dev/null; then
             echo "Installing xeol..."
             mkdir -p ~/tmp/bin
-            curl -sSfL https://raw.githubusercontent.com/xeol-io/xeol/main/install.sh | sh -s -- -b /tmp/bin
+            curl -sSfL https://raw.githubusercontent.com/xeol-io/xeol/main/install.sh | sh -s -- -b ~/tmp/bin
             export PATH=~/tmp/bin:$PATH
         fi
     '''
     
     // Run the xeol scan
-    sh "/tmp/bin/xeol . --output table"  // or json, sarif, etc.
+    sh "~/tmp/bin/xeol . --output table"  // or json, sarif, etc.
 }
