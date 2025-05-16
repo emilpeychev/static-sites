f2a44ca97da10df9efda30550f7f644096d5647c
emilpeychev <emi.peychev@gmail.com>
2025-05-16 14:39:40 +0300

    env fix

--- vars/envStage.groovy
+++ vars/envStage.groovy
@@ -1,6 +1,6 @@
 def call() {
     def branch = env.BRANCH_NAME ?: sh(script: "git rev-parse --abbrev-ref HEAD", returnStdout: true).trim()
-    def isProd = (branch == 'main')
+    def isProd = (branch == 'master')
 
     echo "Environment: ${isProd ? 'Production' : 'Development'} (Branch: ${branch})"
 
