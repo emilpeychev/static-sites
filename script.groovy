68a5577e525e742098e63f10fad44f0f9c1a8189
emilpeychev <emi.peychev@gmail.com>
2025-05-16 14:17:31 +0300

    add branch detection

--- script.groovy
+++ script.groovy
@@ -1,13 +1,16 @@
 def imageName = ''
+// Dynamic Branch detection
+def branchName = env.BRANCH_NAME ?: 'local'
 
-def buildImage() {
+def buildImage() { 
+    def sanitizedBranch = branchName.replaceAll(/[^a-zA-Z0-9.-]/, '-')
+    def timestamp = sh(script: 'date +"%Y%m%d%H%M%S"', returnStdout: true).trim()
+    imageName = "softunium/static-site:${sanitizedBranch}-${timestamp}"
+
     echo "Building the docker image..."
+    echo "Image name: ${imageName}"
+    
     withCredentials([usernamePassword(credentialsId: 'DockerHub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        def timestamp = sh(script: 'date +"%Y%m%d%H%M%S"', returnStdout: true).trim()
-        imageName = "softunium/static-site:rightshift.bg-${timestamp}"
-
-        echo "Image name: ${imageName}"
-        
         sh "docker build -t ${imageName} ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
         sh "docker push ${imageName}"
