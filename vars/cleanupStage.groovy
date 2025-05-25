ee1f515cd520de34945c0d03aad7a7f8a4984824
emilpeychev <emi.peychev@gmail.com>
2025-05-25 14:27:25 +0300

    fix

--- vars/cleanupStage.groovy
+++ vars/cleanupStage.groovy
@@ -1,6 +1,6 @@
 def call(){
         //Clean-up Stage
         echo 'Removing scan tools...'
-        sh "rm -fr ./tmp/bin/trivy"
-        sh "rm -fr ./tmp/bin/xeol"
+        sh "rm -fr /tmp/bin/trivy"
+        sh "rm -fr /tmp/bin/xeol"
 }
